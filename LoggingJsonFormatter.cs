da1640d9905ed8c78110e1da02b35a9b64c7b378
sasireha sundaresan <ssundaresan@artsenzorg.nl>
2020-06-02 16:44:29 +0200

    JsonFormatter initial Commit.

---
+++ LoggingJsonFormatter.cs
@@ -0,0 +1,105 @@
+﻿using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Json;
+using Serilog.Parsing;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace JsonFormatter
+{
+    public class LoggingJsonFormatter : ITextFormatter
+    {
+        readonly JsonValueFormatter _valueFormatter;
+
+        public LoggingJsonFormatter(JsonValueFormatter valueFormatter = null)
+        {
+            _valueFormatter = valueFormatter ?? new JsonValueFormatter(typeTagName: "$type");
+        }
+
+        /// <summary>
+        /// Format the log event into the output. Subsequent events will be newline-delimited.
+        /// </summary>
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            FormatEvent(logEvent, output, _valueFormatter);
+            output.WriteLine();
+        }
+
+        /// <summary>
+        /// Format the log event into the output.
+        /// </summary>
+        public static void FormatEvent(LogEvent logEvent, TextWriter output, JsonValueFormatter valueFormatter)
+        {
+            if (logEvent == null)
+            {
+                throw new ArgumentNullException(nameof(logEvent));
+            }
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+            if (valueFormatter == null)
+            {
+                throw new ArgumentNullException(nameof(valueFormatter));
+            }
+
+            output.Write("{\"time\":\"");
+            output.Write(logEvent.Timestamp.UtcDateTime.ToString("O"));
+            output.Write("\",\"message\":");
+            JsonValueFormatter.WriteQuotedJsonString(logEvent.MessageTemplate.Text, output);
+
+            var tokensWithFormat = logEvent.MessageTemplate.Tokens
+                .OfType<PropertyToken>()
+                .Where(pt => pt.Format != null);
+
+            if (tokensWithFormat.Any())
+            {
+                output.Write(",\"renderings\":[");
+                var delim = "";
+                foreach (var r in tokensWithFormat)
+                {
+                    output.Write(delim);
+                    delim = ",";
+                    var space = new StringWriter();
+                    r.Render(logEvent.Properties, space);
+                    JsonValueFormatter.WriteQuotedJsonString(space.ToString(), output);
+                }
+                output.Write(']');
+            }
+
+            if (logEvent.Level != LogEventLevel.Information)
+            {
+                output.Write(",\"loglevel\":\"");
+                output.Write(logEvent.Level.ToString().ToUpper());
+                output.Write('\"');
+            }
+
+            if (logEvent.Exception != null)
+            {
+                output.Write(",\"exception\":");
+                JsonValueFormatter.WriteQuotedJsonString(logEvent.Exception.Message, output);
+                output.Write(",\"stacktrace\":");
+                JsonValueFormatter.WriteQuotedJsonString(Regex.Replace(logEvent.Exception.StackTrace, @"\t|\n|\r", ""), output);
+            }
+
+            foreach (var property in logEvent.Properties)
+            {
+                var name = property.Key;
+                if (name.Length > 0 && name[0] == '@')
+                {
+                    // Escape first '@' by doubling
+                    name = '@' + name;
+                }
+
+                output.Write(',');
+                JsonValueFormatter.WriteQuotedJsonString(name, output);
+                output.Write(':');
+                valueFormatter.Format(property.Value, output);
+            }
+
+            output.Write('}');
+        }
+    }
+}
